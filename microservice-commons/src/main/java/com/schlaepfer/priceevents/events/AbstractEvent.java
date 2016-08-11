d1c25d2c0e24ccc91582b4c7a303c6e587983b69
Patrick Schläpfer <patrick@schlaepfer.com>
2016-08-11 11:25:12 +0200

    Some small changes

--- microservice-commons/src/main/java/com/schlaepfer/priceevents/events/AbstractEvent.java
+++ microservice-commons/src/main/java/com/schlaepfer/priceevents/events/AbstractEvent.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 public abstract class AbstractEvent implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+	
 	private String id;
 
 	public AbstractEvent() {
