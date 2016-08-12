ab0398355839fd72de50d5247e9223216ef67c6c
Patrick Schlaepfer <patrick@schlaepfer.com>
2016-08-13 00:34:56 +0200

    Added production.ymls

---
+++ docker-push.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker push pschlae/config-service:latest
+docker push pschlae/discovery-service:latest
+docker push pschlae/gateway-service:latest
+docker push pschlae/price-query-side:latest
+docker push pschlae/price-command-side:latest
